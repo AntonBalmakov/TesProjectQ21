2dece08b932203ff986ebd55221010d77009a149
AntonBalmakov <61959404+AntonBalmakov@users.noreply.github.com>
2025-09-15 21:01:38 +0300

    Мой первый коммит

---
+++ first_file.py
@@ -0,0 +1,15 @@
+print('Halabala')
+print('Halabala')
+print('Halabala')
+
+"""
+Создать функицю которая принемает 2 параметра и считает между ними сумму 
+"""
+
+
+def function_sum(a, b):
+    return a + b
+
+
+a = function_sum(5, 6)
+print(a)
